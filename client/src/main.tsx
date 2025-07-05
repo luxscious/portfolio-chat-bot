6305bfef59672d981876463aac6c0922cbf2a78a
Gabriella Gerges <57682940+luxscious@users.noreply.github.com>
2025-07-04 21:46:20 -0300

    Update Icon for Site

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App";
 
+// Dynamically set favicon from env variable
+const favicon = document.querySelector(
+  "link[rel='icon']"
+) as HTMLLinkElement | null;
+if (favicon) {
+  favicon.href = `${import.meta.env.VITE_BLOB_BASE_URL}/g_icon.png`;
+}
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
